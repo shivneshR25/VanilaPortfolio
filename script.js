d307ea684312ef8863772b6acd387dba0f54bd4e
R Shivnesh <shivnesh_shiva@outlook.com>
2025-04-04 19:30:52 +0530

    update

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+function switchdark() {
+    const body = document.body;
+    const icon = document.getElementById("dark-mode-icon");
+
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+        icon.src = "./assets/light.svg"; 
+    } else {
+        localStorage.setItem("darkMode", "disabled");
+        icon.src = "./assets/dark.svg"; 
+    }
+}
